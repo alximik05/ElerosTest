a94b6ed3af06a07b9e7ab4595e8aeeb13878ed9d
stas.grishaev <oalximiko@gmail.com>
2015-05-15 20:22:16 +0300

    add maven dependencies

--- src/com/eleron/Main.java
+++ src/com/eleron/Main.java
@@ -12,20 +12,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-        DeviceRegistry store = new DeviceRegistry();
-        VerificationDevice device = new VerificationDevice( UUID.randomUUID(), "first", "aes", VerificationDeviceType.ADVANCE);
-        VerificationDevice device2 = new VerificationDevice(UUID.randomUUID(), "second", "aes", VerificationDeviceType.SIMPLE);
-        Sensor sensor = new Sensor(UUID.randomUUID(), "first", "aes", true);
-        Sensor sensor2 = new Sensor(UUID.randomUUID(), "second", "aes", false);
-
-
-
-        store.save(device);
-        store.save(device2);
-        store.save(sensor);
-        store.save(sensor2);
-
-        store.showAll();
+//        DeviceRegistry store = new DeviceRegistry();
+//        VerificationDevice device = new VerificationDevice( UUID.randomUUID(), "first", "aes", VerificationDeviceType.ADVANCE);
+//        VerificationDevice device2 = new VerificationDevice(UUID.randomUUID(), "second", "aes", VerificationDeviceType.SIMPLE);
+//        Sensor sensor = new Sensor(UUID.randomUUID(), "first", "aes", true);
+//        Sensor sensor2 = new Sensor(UUID.randomUUID(), "second", "aes", false);
+//
+//
+//
+//        store.save(device);
+//        store.save(device2);
+//        store.save(sensor);
+//        store.save(sensor2);
+//
+//        store.showAll();
     }
 
 
